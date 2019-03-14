b45af1f5b8045f5bb374ebbde279b96470d6db9f
Jig <slowlow1@rambler.ru>
2019-03-15 00:46:52 +0300

    Добавил разделение враппера на header и footer

---
+++ footer.php
@@ -0,0 +1,38 @@
+<div class="col-md-3 p-0">
+					<div class="container p-2 my-auto text-light rounded" style="background:url(technicalissues.jpg);">
+						<h6 class="text-center ">Популярные статьи</h6>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<footer class="contaimer-fluid text-center bg-light" style="height: 50px;
+	background: rgb(213,213,213);
+	background: linear-gradient(180deg, rgba(213,213,213,0) 0%, rgba(59,59,59,0.7511379551820728) 100%);">
+		<div class="row m-0 text-nowrap align-items-center">
+			<div class="col text-center">
+				<div class="row flex-row-reverse">
+					<div class="col">
+						<p class="m-0 my-auto float-md-right">Обратная связь: cocktailbarteam@mail.ru</p>
+					</div>
+					<div class="col">
+						<p class="m-0">copyright 2019 ©</p>
+					</div>
+			</div>
+			</div>
+			<div class="col-auto col-sm-2">
+				<a class="p-0 float-left" href="#"><img src="inst.png"></a>
+			</div>
+					
+						
+			
+		</div>
+		
+
+		
+	</footer>
+
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
@@ -8,9 +8,8 @@
 		html, body {
 				    height: 100%;
 				    min-height: 100%;}
-	    #background{
+		#background{
 	    	background-color:#7FFFD4;
-	    }
 	</style>
 </head>
 <body>
@@ -44,49 +43,3 @@
 				</div>
 			</div>
 		</header>
-		<div class="contaimer-fluid mx-xl-4 mx-lg-3 mx-md-2 p-4">
-			<div class="row m-0" >
-				<div class="col-md-9 p-0 pr-md-2 m-0">
-					<div class="container mb-2 p-2 p-sm-3 p-md-4 rounded bg-info text-center  text-white" >						
-						<?php include_once 'MISHA_article_page.php' ?>
-					</div>			
-					</div>
-					<div class="col-md-3 p-0">
-					<div class="container p-2 my-auto text-light rounded " style="background:url(technicalissues.jpg);">
-						<h6 class="text-center">Популярные статьи</h6>
-					</div>
-				</div>
-				</div>
-				
-			</div>
-			
-		</div>
-	</div>
-	<footer class="contaimer-fluid text-center bg-light" style="height: 50px;
-	background: rgb(213,213,213);
-	background: linear-gradient(180deg, rgba(213,213,213,0) 0%, rgba(59,59,59,0.7511379551820728) 100%);">
-		<div class="row m-0 text-nowrap align-items-center">
-			<div class="col text-center">
-				<div class="row flex-row-reverse">
-					<div class="col">
-						<p class="m-0 my-auto float-md-right">Обратная связь: cocktailbarteam@mail.ru</p>
-					</div>
-					<div class="col">
-						<p class="m-0">copyright 2019 ©</p>
-					</div>
-			</div>
-			</div>
-			<div class="col-auto col-sm-2">
-				<a class="p-0 float-left" href="#"><img src="inst.png"></a>
-			</div>
-					
-						
-			
-		</div>
-		
-
-		
-	</footer>
-
-</body>
-</html>
