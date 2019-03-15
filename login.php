5865cc65b8a7d01b835147551b894709d73d186e
Alexander <cherryknight@yandex.ru>
2019-03-16 01:09:46 +0300

    Добавлена некрасивая страница авторизации с соответствующей формой

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php include "header.php" ?>
+<h3 class="text-center pb-3">Авторизация</h3>
+<form action="" class="text-center">
+  	<div class="form-group">
+    	<input type="text" class="form-control" id="login" placeholder="Имя пользователя">
+	</div>
+	<div class="form-group">
+	    <input type="password" class="form-control" id="password" placeholder="Пароль" >
+	</div>
+	<div class="form-group">
+		<a href="#">Впервые на сайте?</a>
+	</div>
+ 	<button type="submit" class="btn btn-primary">Войти</button>
+</form>
+
+<?php include "footer.php" ?>
