91bbbca11f89b8faa7de7c0e1eed949348f6952b
Alexander <cherryknight@yandex.ru>
2019-03-16 18:45:27 +0300

    Добавлена главная страница. Является обработчиком POST-данных из формы авторизации, логика перенесена из удаленного файла authorization.php

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+include 'connect_function.php';
+if (!empty($_POST["login"]) && !empty($_POST["password"]))
+	{$link=connect();
+	$result=mysqli_query($link,"SELECT id FROM user WHERE log = '".$_POST["login"]."' AND password='".$_POST["password"]."'limit 1;") or die("...".mysqli_error($link));
+	if (mysqli_num_rows($result)){
+		session_start();
+		$row=mysqli_fetch_assoc($result);
+		$_SESSION['user'] = $row['id'];
+					}
+	else
+		{header("Location: login.php");
+		exit;}
+
+}
+else
+	echo "error";
+include "header.php";
+include 'footer.php';
+?>
