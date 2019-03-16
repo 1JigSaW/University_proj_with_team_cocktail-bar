48010bf48ec260d072fb2020d6a9c6fe91ad6335
Alexander <cherryknight@yandex.ru>
2019-03-16 19:17:22 +0300

    Вернул случайно убранный метод=ПОСТ у формы логина

--- login.php
+++ login.php
@@ -1,16 +1,15 @@
 <?php include "header.php" ?>
 <h3 class="text-center pb-3">Авторизация</h3>
-<form action="" class="text-center">
+<form method="POST" action="../main.php" class="text-center">
   	<div class="form-group">
-    	<input type="text" class="form-control" id="login" placeholder="Имя пользователя">
+    	<input type="text" class="form-control" name="login" placeholder="Имя пользователя">
 	</div>
 	<div class="form-group">
-	    <input type="password" class="form-control" id="password" placeholder="Пароль" >
+	    <input type="password" class="form-control" name="password" placeholder="Пароль" >
 	</div>
 	<div class="form-group">
 		<a href="#">Впервые на сайте?</a>
 	</div>
  	<button type="submit" class="btn btn-primary">Войти</button>
 </form>
-
 <?php include "footer.php" ?>
