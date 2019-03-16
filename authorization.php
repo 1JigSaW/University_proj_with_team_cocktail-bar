dd8fc3e703e8f4071e6a6647ced0cf4d998b7ed5
Alexander <cherryknight@yandex.ru>
2019-03-16 15:02:24 +0300

    Добавлена регистарция сессионнной переменной в случае, если пользователь с указанными при входе логином/паролем есть в бд

--- authorization.php
+++ authorization.php
@@ -4,7 +4,7 @@ if (!empty($_POST["login"]) && !empty($_POST["password"]))
 	{$link=connect();
 	$result=mysqli_query($link,"SELECT id FROM user WHERE log = '".$_POST["login"]."' AND password='".$_POST["password"]."'limit 1;") or die("...".mysqli_error($link));
 	if (mysqli_num_rows($result))
-		echo 'успех';
+		$_SESSION['user'] = mysqli_fetch_assoc($result)['id'];
 	else
 		{header("Location: login.php");
 		exit();}
