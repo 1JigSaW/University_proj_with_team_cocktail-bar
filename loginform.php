7b8319ae5674adc936b0a9ffe4847acc0bfb0473
Alexander <cherryknight@yandex.ru>
2019-03-17 10:17:11 +0300

    Форма логина перенесена в отдельный файл

---
+++ loginform.php
@@ -0,0 +1,12 @@
+<form method="POST" action="../main.php" class="text-center">
+  	<div class="form-group">
+    	<input type="text" class="form-control" name="login" placeholder="Имя пользователя">
+	</div>
+	<div class="form-group">
+	    <input type="password" class="form-control" name="password" placeholder="Пароль" >
+	</div>
+	<div class="form-group">
+		<a href="#">Впервые на сайте?</a>
+	</div>
+ 	<button type="submit" class="btn btn-primary">Войти</button>
+</form>
