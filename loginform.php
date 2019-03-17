6962daeed608c55b0c29ffa161ba3f49433946af
Alexander <cherryknight@yandex.ru>
2019-03-17 12:43:53 +0300

    Сделал поля логина/пароля обязательными

--- loginform.php
+++ loginform.php
@@ -1,9 +1,9 @@
 <form method="POST" action="../main.php" class="text-center">
   	<div class="form-group">
-    	<input type="text" class="form-control" name="login" placeholder="Имя пользователя">
+    	<input type="text" class="form-control" required name="login" placeholder="Имя пользователя">
 	</div>
 	<div class="form-group">
-	    <input type="password" class="form-control" name="password" placeholder="Пароль" >
+	    <input type="password" class="form-control" required name="password" placeholder="Пароль" >
 	</div>
 	<div class="form-group">
 		<a href="#">Впервые на сайте?</a>
