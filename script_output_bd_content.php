ed2e3c85dfb1685541161fa146415466cc934fcb
Mihail Duev <misho_1998@mail.ru>
2019-03-17 18:50:31 +0000

    Исправил соотношение к id статьи в выводе данных контента

--- script_output_bd_content.php
+++ script_output_bd_content.php
@@ -1,6 +1,6 @@
                                                 <?php 
 							$id=$_GET['page'];
-							$text = mysqli_query($link,"SELECT * FROM `content` WHERE $id=`id` ")or die(mysqli_error());	
+							$text = mysqli_query($link,"SELECT * FROM `content` WHERE $id=`article_id` ")or die(mysqli_error());	
 							while($arr = mysqli_fetch_assoc($text)){
         					echo "<div class=\"container-fluid bg-info m-3 \">";
         					echo $arr['text_article'];
