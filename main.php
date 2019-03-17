3b90c5d2b514d4c0a3d40cea5e6f4669504ac264
Alexander <cherryknight@yandex.ru>
2019-03-17 10:23:07 +0300

    Сесионная переменная теперь так же запоминает логин пошльзователя, а не только его id

--- main.php
+++ main.php
@@ -2,11 +2,12 @@
 include 'connect_function.php';
 if (!empty($_POST["login"]) && !empty($_POST["password"]))
 	{$link=connect();
-	$result=mysqli_query($link,"SELECT id FROM user WHERE log = '".$_POST["login"]."' AND password='".$_POST["password"]."'limit 1;") or die("...".mysqli_error($link));
+	$result=mysqli_query($link,"SELECT id,log FROM user WHERE log = '".$_POST["login"]."' AND password='".$_POST["password"]."'limit 1;") or die("...".mysqli_error($link));
 	if (mysqli_num_rows($result)){
 		session_start();
 		$row=mysqli_fetch_assoc($result);
 		$_SESSION['user'] = $row['id'];
+		$_SESSION['log'] = $row['log'];
 					}
 	else
 		{header("Location: login.php");
