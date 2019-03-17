a7beb52fa9ad59259b4ec917aaf161cb921e32f2
Alexander <cherryknight@yandex.ru>
2019-03-17 10:18:01 +0300

    Форма авторизации на странице логина заменяется надписью об ошибке в случае, если он уже авторизован

--- login.php
+++ login.php
@@ -1,15 +1,9 @@
 <?php include "header.php" ?>
 <h3 class="text-center pb-3">Авторизация</h3>
-<form method="POST" action="../main.php" class="text-center">
-  	<div class="form-group">
-    	<input type="text" class="form-control" name="login" placeholder="Имя пользователя">
-	</div>
-	<div class="form-group">
-	    <input type="password" class="form-control" name="password" placeholder="Пароль" >
-	</div>
-	<div class="form-group">
-		<a href="#">Впервые на сайте?</a>
-	</div>
- 	<button type="submit" class="btn btn-primary">Войти</button>
-</form>
+<?php 
+if (!$_SESSION['user']) 
+	include "loginform.php";
+else
+	echo '<p class="text-center">Страница доступна только неавторизованным пользователям.</p>';
+?>
 <?php include "footer.php" ?>
