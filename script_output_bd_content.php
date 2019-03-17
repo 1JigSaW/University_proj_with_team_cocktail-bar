e312da86d0bd218359f93360e40de54349644569
Jig <slowlow1@rambler.ru>
2019-03-17 21:08:02 +0300

    Добавил скрипт(часть) вывода информации из таблицы content

---
+++ script_output_bd_content.php
@@ -0,0 +1,15 @@
+                                                <?php 
+							$id=$_GET['page'];
+							$text = mysqli_query($link,"SELECT * FROM `content` WHERE $id=`id` ")or die(mysqli_error());	
+							while($arr = mysqli_fetch_assoc($text)){
+        					echo "<div class=\"container-fluid bg-info m-3 \">";
+        					echo $arr['text_article'];
+        					echo "<br>";
+        					echo $arr['img_content'];
+        					echo "<br>";
+        					echo $arr['links'];
+        					echo "</div>";
+
+        				}
+						mysqli_close($link);
+							 ?>
