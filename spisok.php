f2f557942c8f14aef1929d7254c5a791c8651433
Jig <slowlow1@rambler.ru>
2019-03-17 20:27:21 +0300

    Изменил ссылку в списке статей

--- spisok.php
+++ spisok.php
@@ -1,31 +1,24 @@
 		<?php include "header.php" ?>
 		<?php include "connect_bd.php" ?>
-		<div class="contaimer-fluid mx-xl-4 mx-lg-3 mx-md-2 p-4">
-			<div class="row m-0">
-				<div class="col-md-9 p-0 pr-md-2 m-0">
-					<div class="container mb-2 p-2 p-sm-3 p-md-4 bg-white rounded">
-						<h1 class="display-2">Список статей</h1>
-
-						<?php 
+					<div>
+						<h1 class="display-2">Список статей</h1>		
+						<?php 						
 						 $link = mysqli_connect($host,$user,$password,$db_name) 
-        				or die("Ошибка " . mysqli_error($link)); 
-        				$res = mysqli_query($link,"SELECT article_id,text_article,img_content,links FROM `content`")or die(mysqli_error());
+        				or die("Ошибка " . mysqli_error($link));
+        				mysqli_set_charset($link,'utf8');       				
+        				$res = mysqli_query($link,"SELECT * FROM `cocktail`")or die(mysqli_error());
+
         				while($row = mysqli_fetch_assoc($res)){
-        					echo "<div><a href=\"complete_script_comment.php\" class=\"container-fluid m-3 \">";
-        					echo $row['text_article'];
+        					echo "<div><a class='container-fluid m-3' href='complete_script_comment.php?page=". $row['id']. "'>";
+        					$id=$row['id'];
+        					echo $row['title_coctail'];
         					echo "</a></div>";
 
         				}
 
 						mysqli_close($link);
-
-
-
-        				
-						?>
-					</div>				
-				</div>
-			</div>
-		</div>
+						?> 
+					</div>
 				<?php include "footer.php" ?>
+				
 				
