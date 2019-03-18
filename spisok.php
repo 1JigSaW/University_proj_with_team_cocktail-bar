823f7da2dc4ac213309b0b1ef0177dc618fbf81e
Jig <slowlow1@rambler.ru>
2019-03-19 02:03:20 +0300

    Добавил готовую страницу со скриптом:список статей

--- spisok.php
+++ spisok.php
@@ -1,22 +1,19 @@
 		<?php include "header.php" ?>
 		<?php include "connect_bd.php" ?>
 					<div>
-						<h1 class="display-2">Список статей</h1>		
+						<h1 class="display-4 text-center ">СПИСОК ВСЕХ ДОСТУПНЫХ СТАТЕЙ</h1>		
 						<?php 						
-						 $link = mysqli_connect($host,$user,$password,$db_name) 
-        				or die("Ошибка " . mysqli_error($link));
-        				mysqli_set_charset($link,'utf8');       				
-        				$res = mysqli_query($link,"SELECT * FROM `cocktail`")or die(mysqli_error());
+						$res = mysqli_query($connect,"SELECT * FROM `cocktail`")or die(mysqli_error());
 
         				while($row = mysqli_fetch_assoc($res)){
-        					echo "<div><a class='container-fluid m-3' href='complete_script_comment.php?page=". $row['id']. "'>";
+        					echo "<div class=\"text-center\"><a class='container-fluid h1' href='complete_script_comment.php?page=". $row['id']. "'>";
         					$id=$row['id'];
         					echo $row['title_coctail'];
         					echo "</a></div>";
 
         				}
 
-						mysqli_close($link);
+						mysqli_close($connect);
 						?> 
 					</div>
 				<?php include "footer.php" ?>
