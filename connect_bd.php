4a562cff755535fb75876c7f046427a13ece69e8
Mihail Duev <misho_1998@mail.ru>
2019-03-18 22:18:50 +0000

    Исправил описки и привел в нужный вид connect_bd

--- connect_bd.php
+++ connect_bd.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>DB</title>
-</head>
-<body>
-
 
 <?php 
 echo "Connect data_base";
@@ -12,24 +5,9 @@ $host='localhost';
 $user='root';
 $password='';
 $db_name='db_coctailbar';
-
 $connect=mysqli_connect($host,$user,$password,$db_name)
 or die("Ошибка " . mysqli_error($connect));
- mysqli_set_charset($link,'utf8');
-/*
-mysqli_select_db($db_name,$connect);
-
-if($connect=true){
-    echo "lol";
-}
-
-else {echo "nooooooooooooooooooooooo";}
-*/
-/*$connect = mysqli_connect ($host, $user, $password) or die(mysqli_error());
- 
-mysqli_select_db($db_name) or die(mysqli_error())
-*/
+ mysqli_set_charset($connect,'utf8');
  ?>
-</body>
-</html>
+
 
