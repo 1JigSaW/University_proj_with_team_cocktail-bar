d05009f535918fd972b6d858c80ebfa40d622e38
Jig <slowlow1@rambler.ru>
2019-03-20 00:23:54 +0300

    Исправил недочет в данной функции

--- connect_function.php
+++ connect_function.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 function connect(){
+
 	$host='localhost';
 	$user='root';
 	$password='';
 	$db_name='db_coctailbar';
 	$connect=mysqli_connect($host,$user,$password,$db_name)	or die("Ошибка " . mysqli_error($connect));
 	if($connect)
-		return $connect;}
-	mysqli_set_charset($link,'utf8');
+		return $connect;
+ mysqli_set_charset($connect,'utf8');}
+	
  ?>
