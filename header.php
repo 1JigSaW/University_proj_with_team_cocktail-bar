09c1a14193af70b1dd5f8adc5f47f1bf719cc4e5
Denis Suetov <den.suetov@mail.ru>
2019-03-23 15:44:26 +0300

    Исправил ошибку, связанную с нажатием на кнопку "Вход"

--- header.php
+++ header.php
@@ -40,7 +40,7 @@ if(@$_GET['do'] == 'logout'){
 					<div class="container text-white text-center d-none d-sm-none d-md-block p-0 m-0">
 					<?php
 						if (!@$_SESSION['user']){
-							echo '<a href="../login.php" role="button" class="btn  border-white bg-primary rounded text-white mr-1" style="">Вход</a>';
+							echo '<a href="login.php" role="button" class="btn  border-white bg-primary rounded text-white mr-1" style="">Вход</a>';
 							echo '<a href="#" role="button" class="btn  border-white bg-primary rounded text-white">Регистрация</a>';}
 						else
 							{$logout='<a href="?do=logout" role="button" class="p-0 m-0 btn btn-lg text-white"><span class="oi oi-account-logout"></span></a>';
