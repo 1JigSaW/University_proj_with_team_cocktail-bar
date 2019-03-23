80ac0122c67179164e3828286c04b243a82e8806
Denis Suetov <den.suetov@mail.ru>
2019-03-23 15:29:25 +0300

    Удалил лишную строку вывода в файле connect_bd.php

--- connect_bd.php
+++ connect_bd.php
@@ -1,13 +1,11 @@
-
 <?php 
-echo "Connect data_base";
+
 $host='localhost';
 $user='root';
 $password='';
 $db_name='db_coctailbar';
-$connect=mysqli_connect($host,$user,$password,$db_name)
-or die("Ошибка " . mysqli_error($connect));
- mysqli_set_charset($connect,'utf8');
+
+$connect=mysqli_connect($host,$user,$password,$db_name) or die("Ошибка " . mysqli_error($connect));
+mysqli_set_charset($connect,'utf8');
+
  ?>
-
-
