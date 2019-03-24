2238a1cd9d0fe2607143c96259c0ba1bc0adda28
Alexander <cherryknight@yandex.ru>
2019-03-24 14:34:50 +0300

    Забыл фигурные скобки

--- connect_function.php
+++ connect_function.php
@@ -1,14 +1,13 @@
 <?php
 function connect(){
-
 	$host='localhost';
 	$user='root';
 	$password='';
 	$db_name='db_coctailbar';
-	$connect=mysqli_connect($host,$user,$password,$db_name)	or die("Ошибка " . mysqli_error($connect));
+	$connect=mysqli_connect($host,$user,$password)	or die("Ошибка " . mysqli_error($connect));
 	if($connect)
-		mysqli_set_charset($connect,'utf8');
-		return $connect;
+		{mysqli_set_charset($connect,'utf8');
+		return $connect;}
  }
 	
  ?>
