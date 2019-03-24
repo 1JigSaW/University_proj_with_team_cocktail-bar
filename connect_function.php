fb68678f9ea6ce33e4c4f3797e57f847c175e883
Denis Suetov <den.suetov@mail.ru>
2019-03-25 01:29:57 +0300

    Вернул имя базы данных на место

--- connect_function.php
+++ connect_function.php
@@ -4,7 +4,7 @@ function connect(){
 	$user='root';
 	$password='';
 	$db_name='db_coctailbar';
-	$connect=mysqli_connect($host,$user,$password)	or die("Ошибка " . mysqli_error($connect));
+	$connect=mysqli_connect($host,$user,$password,$db_name)	or die("Ошибка " . mysqli_error($connect));
 	if($connect)
 		{mysqli_set_charset($connect,'utf8');
 		return $connect;}
