96db471a937e392a2a16b8f2a463b2927569a505
Denis Suetov <den.suetov@mail.ru>
2019-03-26 03:02:01 +0300

    В файл-обработчик главной страницы добавлены сообщения об ошибках и код обработки нажатия на кнопки "добавить"

--- isearch.php
+++ isearch.php
@@ -3,12 +3,45 @@
 
 <?php
 
+if (isset($_POST['add']))
+{
+	if($_POST['add'] == "drink")
+	{
+		$_SESSION['tmp'] = $_POST['product'];
+		if(!$_POST['drink'])
+		{
+			header("Location: main.php?error=drink");
+			exit;
+		}
+	}
+	if($_POST['add'] == "product")
+	{
+		$_SESSION['tmp'] = $_POST['drink'];
+		if(!$_POST['product'])
+		{
+		header("Location: main.php?error=product");
+		exit;
+		}
+	}
+	header ("Location: main.php?id=" . $_POST[$_POST['add']] . "&type=" . $_POST['add']);
+}
+
 print_r($_POST);
 
-$result = mysqli_query($connect, "SELECT title_product FROM `product` WHERE `id` = " . $_POST['product1']);
+$result = mysqli_query($connect, "SELECT title_product FROM `product` WHERE `id` = " . $_POST['drink']);
 $name = mysqli_fetch_assoc($result);
 
-echo "<br>В первом столбце был выбран продукт из базы данных под названием: " . $name['title_product'];
+echo "<br><br>В первом столбце был выбран продукт из базы данных под названием: ";
+if($_POST['drink'])
+echo $name['title_product'];
+else
+echo "[ПРОДУКТ НЕ БЫЛ ВЫБРАН]";
+
+echo "<br><br>Во втором столбце был выбран продукт из списка под названием: ";
+if($_POST['product'])
+echo $_POST['product'];
+else
+echo "[ПРОДУКТ НЕ БЫЛ ВЫБРАН]";
 
 ?>
 
