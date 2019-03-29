8273d539f43c97444b2cb1876e62c12a0e28b6fd
Jig <slowlow1@rambler.ru>
2019-03-30 01:21:54 +0300

    Исправил ошибку с выводом комментариев, готова рабочая полноценная страница со стаьей

--- article.php
+++ article.php
@@ -1,5 +1,6 @@
-﻿<?php header('Content-Type: text/html; charset=utf-8');
-mysql_query("SET NAMES 'UTF8'");
+<?php 
+mysql_set_charset("UTF-8");
+
 ?>
 <?php include "header.php" ?>
 <?php include "connect_bd.php" ?>
@@ -53,109 +54,106 @@ if(isset($_GET['page'])){
 						<div class="container-fluid text-right ">
 							<a href="<?php echo $arr['links'];}?>" class="h4 text-white">ССЫЛКА</a>
 						</div>
-					</div>
 
 
-					<form action="" method="post">
-
-						<input class="btn btn-primary" type="submit" name="like" value="Мне понравилось" >
-						<input type='hidden' name='dislike' value='' >
-						<input class="btn btn-primary" type="submit" name="dislike" value="Мне не понравилось">	
-					</form>  
-
-
-					<?php 
-			// $user_id=$_SESSION['user'];
-			// $input=$_SESSION['log'];
-			// $a = mysqli_query($connect,"SELECT * FROM `article` ")or die(mysqli_error());
-			// while($arr = mysqli_fetch_assoc($a)){	
-
-			// if($user_id!='user_id'){
-			// if($_POST['like']){
-
-			// 	$like=$_POST['like'];
-			// 	$query ="UPDATE article SET rating=rating+1,  user_id=$user_id WHERE $id=id ";
-			// 	$result = mysqli_query($connect, $query) or die("Ошибка " . mysqli_error($connect));
-			// }
-			// else {echo "stop";}
-
-
-
-			// else if($_POST['dislike']){
-			// 	$dislike=$_POST['dislike'];
-			// 	$query ="UPDATE article SET rating = rating-1  WHERE $id=`id` ";
-			// 	$result = mysqli_query($connect, $query) or die("Ошибка " . mysqli_error($connect));
-			// }
-
-
-					?>
-
-					<!-- <?php 
-					$article = mysqli_query($connect,"SELECT * FROM `rating` WHERE $id=`id`")or die(mysqli_error());	
-					while($arr = mysqli_fetch_assoc($article)){?>
-						<div class="container-fluid text-center">
-							<div  class="display-4"><?php echo $arr['rating'];}?></div>
-						</div>
-					-->
-					<?php
-
-					$id=$_GET['page'];
-					$input=$_SESSION['log'];
-					$user_id=$_SESSION['user'];
-					$data=date("Y-m-d H:i:s", (time()-60*60));
-					$id=$_GET['page'];
-					
-					if(isset($_POST['comment'])){
-    								// экранирования символов для mysql
-						$comment = htmlentities(mysqli_real_escape_string($connect, $_POST['comment']));
-						$query ="INSERT INTO  comment VALUES(NULL,'$comment','$data','$id' ,'$user_id')";
-   								 // выполняем запрос
-						$result = mysqli_query($connect, $query) or die("Ошибка " . mysqli_error($connect)); }
-						if($result=true)
-						{
-							echo "<span style='color:blue;'>Данные добавлены</span>";
-						}
-						?>
-						<div>
-							<form action="" method="post" >
-								<div class="m-2"><input class="form-control form-control-lg rounded " type="text" placeholder="Введите ваш комментраий" name="comment"></div>
-								<div class="container"></div>
-								<div class="row"></div>
-								<div class="m-2"><input class="btn btn-primary p-1 text-center col-12" type="submit" value="Добавить комментарий"></div>
-
-							</form>
-						</div>
 						<?php 
+					$a=0;  /////Переменная для вывода одиночной информации, а не набора из цикла
+					$user_id=$_SESSION['user'];//////
+					$input=$_SESSION['log'];
+					if($_POST['like'] || $_POST['dislike']){
+						$us="SELECT  `user_id`,`article_id` FROM `rating` ";
+						$res = mysqli_query($connect,$us)or die(mysqli_error());
 
-						// $res = mysqli_query($connect,"SELECT id,log FROM `user`  ")or die(mysqli_error());
-						// while($row = mysqli_fetch_assoc($res)){
-						// 	if($user_id==$row['id']){
-						// 	$user_id=$row['log'];}
-						$res = mysqli_query($connect,"SELECT * FROM `comment` JOIN `user` WHERE `user`.`id`=`comment`.`user_id`")or die(mysqli_error());
-
-
-
-						while($row = mysqli_fetch_assoc($res)) {
-							if($id==$row['article_id']    ){
-
-								echo "<div class=\"bg-info rounded  container text-white  \">";
-								echo "<p></p>";
-								echo "<div class=\"container-fluid \"></div>";
-								echo "<div class=\"\"></div>";
-								echo $row['log'];
-								echo "<div class=\"container-fluid  h4\">";	
-								echo $row['text_comment'];
-								echo "</div>";
-								echo "<div class=\"text-right\">";
-
-								echo $row['data_comment'];
+						while($arr = mysqli_fetch_assoc($res)){
+							
+							if($user_id==$arr['user_id'] && $id==$arr['article_id']){
 
 
+								echo "<div class=\"alert alert-success text-center\" role=\"alert\">
+								<h4 class=\"alert-heading\">";
+								echo $input; 
+								echo "</h4> Вы уже оставляли свой голос! </div>";
+								$a=1;
+								break;}
 
-								echo "</div>";
-								echo "</div>";}}
-								mysqli_close($connect);
-								?>
+							}
+							if($a!=1){	
+								if($_POST['like']){
+									$plus=1;
+									$query ="INSERT INTO  rating VALUES(NULL,'$user_id','$plus','$id')";}
+									if($_POST['dislike']){
+										$minus=-1;
+
+										$query ="INSERT INTO  rating VALUES(NULL,'$user_id','$minus','$id')";}
+										$result = mysqli_query($connect, $query) or die("Ошибка " . mysqli_error($connect));
+										echo "<div class=\"alert alert-success text-center\" role=\"alert\">
+										<h4 class=\"alert-heading\">";
+										echo $input; 
+										echo "</h4> Спасибо за вашу активность! </div>";}	}
+										?>
+
+										<?php 
+										$article = mysqli_query($connect,"SELECT SUM(`sum`) as sum FROM `rating` WHERE $id=`article_id`")or die(mysqli_error());	
+										while($arr = mysqli_fetch_assoc($article)){?>
+											<!-- <form action="" method="post" class="row p-3">
 
 
-								<?php include "footer.php" ?>
+												<input class="btn btn-outline-dark col" type="submit" name="dislike" value="DISLIKE">
+												<div  class="display-5 col-6 text-center alert alert-info m-3" role="alert">ТЕКУЩИЙ РЕЙТИНГ:<?php echo $arr['sum'];}?></div>
+												<input class="btn btn-outline-light col" type="submit" name="like" value="LIKE" >
+
+											</form>   -->
+										</div>
+
+
+										<?php
+
+										$id=$_GET['page'];
+										$input=$_SESSION['log'];
+										$user_id=$_SESSION['user'];
+										$data=date("Y-m-d H:i:s", (time()-60*60));
+
+
+										if(isset($_POST['comment'])){
+    								// экранирования символов для mysql
+											$comment = htmlentities(mysqli_real_escape_string($connect, $_POST['comment']));
+											$query ="INSERT INTO  comment VALUES(NULL,'$comment','$data','$id' ,'$user_id')";
+   								 // выполняем запрос
+											$result = mysqli_query($connect, $query) or die("Ошибка " . mysqli_error($connect)); }
+
+											?>
+											<div>
+												<form action="" method="post" >
+													<div class="m-2"><input class="form-control form-control-lg rounded " type="text" placeholder="Введите ваш комментраий" name="comment"></div>
+													<div class="container"></div>
+													<div class="row"></div>
+													<div class="m-2"><input class="btn btn-primary p-1 text-center col-12" type="submit" value="Добавить комментарий"></div>
+
+												</form>
+											</div>
+											<?php 
+
+
+											$res = mysqli_query($connect,"SELECT * FROM `comment` JOIN `user` WHERE `user`.`id`=`comment`.`user_id` ORDER BY `comment`.`id` asc")or die(mysqli_error());
+
+											while($row = mysqli_fetch_assoc($res)) {
+												if($id==$row['article_id'] ){
+
+													echo "<div class=\"bg-info rounded  container text-white  \">";
+													echo "<p></p>";
+													echo "<div class=\"container-fluid \"></div>";
+													echo "<div class=\"\"></div>";
+													echo $row['log'];
+													echo "<div class=\"container-fluid  h4\">";	
+													echo $row['text_comment'];
+													echo "</div>";
+													echo "<div class=\"text-right\">";
+
+													echo $row['data_comment'];
+													echo "</div>";
+													echo "</div>";}}
+													mysqli_close($connect);
+													?>
+
+
+													<?php include "footer.php" ?>
