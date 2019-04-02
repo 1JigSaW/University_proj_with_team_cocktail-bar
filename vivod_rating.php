91c260b1629d2d696d33a652a25ef65fe1c60dad
Jig <slowlow1@rambler.ru>
2019-04-02 20:20:32 +0300

    eger

--- vivod_rating.php
+++ vivod_rating.php
@@ -1,9 +1,6 @@
-<?php 
-$article = mysqli_query($connect,"SELECT SUM(`sum`) as sum FROM `rating` WHERE $id=`article_id`")or die(mysqli_error());	
-while($arr = mysqli_fetch_assoc($article)){?>
-	<form action="" method="post" class="row p-3">
-		<input class="btn btn-outline-dark col" type="submit" name="dislike" value="DISLIKE">
-		<div  class="display-5 col-6 text-center alert alert-info m-3" role="alert">ТЕКУЩИЙ РЕЙТИНГ:<?php echo $arr['sum'];}?></div>
-		<input class="btn btn-outline-light col" type="submit" name="like" value="LIKE" >
-	</form>  
 <?php
+
+function rating($id,$connect){
+	$arr = mysqli_query($connect,"SELECT SUM(`sum`) as sum FROM `rating` WHERE $id=`article_id`")or die(mysqli_error());	
+	return $arr['sum'];}
+	?>
