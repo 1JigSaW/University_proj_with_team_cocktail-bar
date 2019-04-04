eb100b87974b41e892bf731ff83b7aa7bda378ee
Alexander <cherryknight@yandex.ru>
2019-04-05 02:47:00 +0300

    Чуть облагородил популярное

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 </div>		
 				</div>
 				<div class="col-md-3 p-0">
-					<div class="container p-2 my-auto text-white rounded" style="min-width:200px;background:url(img/technicalissues.jpg);">
+					<div class="container p-2 my-auto text-white rounded bg-success" style="min-width:200px;">
 						<h6 class="text-center ">Популярные статьи</h6>
 						<div class="row p-0 mx-2 my-2">
 							<?php
 								include "random_articles.php";
 								$query=
-										"<a class='col col-md-12 %s text-center' href='/article.php?page=%d'><div class='row align-items-center text-white' style='height:144px; background:url(img/%s);background-size: cover;background-position: center;'><div class='col item'>%s</div></div></a>
+										"<a class='col col-md-12 %s text-center popular' href='/article.php?page=%d'><div class='row align-items-center text-white' style='height:144px; background:url(img/%s);background-size: cover;background-position: center;'><div class='col item'><p style='text-shadow:black 1px 1px 0, black -1px -1px 0,black -1px 1px 0, black 1px -1px 0;'>%s</p></div></div></a>
 										";
 								$m=array('mr-2 mr-md-0 mb-md-2','ml-2 ml-md-0 mt-md-2');
 								for($i=0;$i<2;$i++){
