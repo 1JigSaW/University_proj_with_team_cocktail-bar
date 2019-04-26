2ede7231204896cc8dcf4c9ff4b991d897369bf6
Alexander <cherryknight@yandex.ru>
2019-04-26 22:46:52 +0300

    ав

--- spisok.php
+++ spisok.php
@@ -1,12 +1,12 @@
-﻿<?php include "header.php" ?>
-<?php include "connect_bd.php" ?>
+﻿<?php include "header.php"?>
 <div>
 	<h1 class="display-4 text-center ">СПИСОК ВСЕХ ДОСТУПНЫХ СТАТЕЙ</h1>		
-	<?php 						
+	<?php
+	include "connect_bd.php";		
 	$res = mysqli_query($connect,"SELECT * FROM `cocktail`")or die(mysqli_error());
 
 	while($row = mysqli_fetch_assoc($res)){
-	echo "<div class=\"text-center\" ><a class='container-fluid h1' href='article-". $row['id']. "'>";
+	echo "<div class=\"text-center\" ><a class='container-fluid h1' href='article_". $row['id']. "'>";
 	$id=$row['id'];
 	echo $row['title_coctail'];
 	echo "</a></div>";
