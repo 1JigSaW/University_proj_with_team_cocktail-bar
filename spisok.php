a9bfc9812e92b95772c47d7ffe412ea4907785cf
Jig <slowlow1@rambler.ru>
2019-04-29 23:07:07 +0300

    Исправил ошибку,потому что файл подключения был поставлен не сверху, и сохранил без ВОМ

--- spisok.php
+++ spisok.php
@@ -1,20 +1,21 @@
-﻿<?php include "header.php"?>
+<?php include "header.php";
+include "connect_bd.php";?>
 <div>
 	<h1 class="display-4 text-center ">СПИСОК ВСЕХ ДОСТУПНЫХ СТАТЕЙ</h1>		
 	<?php
-	include "connect_bd.php";		
+	
 	$res = mysqli_query($connect,"SELECT * FROM `cocktail`")or die(mysqli_error());
 
 	while($row = mysqli_fetch_assoc($res)){
-	echo "<div class=\"text-center\" ><a class='container-fluid h1' href='article_". $row['id']. "'>";
-	$id=$row['id'];
-	echo $row['title_coctail'];
-	echo "</a></div>";
+		echo "<div class=\"text-center\" ><a class='container-fluid h1' href='article_". $row['id']. "'>";
+		$id=$row['id'];
+		echo $row['title_coctail'];
+		echo "</a></div>";
 
-    }
+	}
 
-     mysqli_close($connect);
-    ?> 
+	mysqli_close($connect);
+	?> 
 </div>
 <?php include "footer.php" ?>
 
