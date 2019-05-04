4de48f5856a41af49b4d00190f8d02dc44964305
thienlao <tiger07992222@gmail.com>
2019-05-04 14:03:33 +0300

    Добавил проверку на совпадение с данными в БД

--- registration.php
+++ registration.php
@@ -2,48 +2,68 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title>Регистрация</title>
+	
+	<title></title>
 </head>
+<style >
+.reg{
+	max-width: 400px;
+	padding: 15px;
+	margin: 0 auto;
+
+}
+</style>
 <body>
+<?php 
+include ('connect_bd.php');
+if (isset($_POST['log']) && isset($_POST['password']))
+{
+	
+	$data_born=$_POST['data_born'];
+	$log=$_POST['log'];
+	$password=$_POST['password'];
+$s="SELECT * FROM user WHERE log = '$log'";
+$res=mysqli_query( $connect, $s);
+$num = mysqli_num_rows($res);
+if($num == 0) {
 
-<?php
-
-require ('connect_bd.php');
-$select_db=mysqli_select_db($connect,'db_coctailbar');
-if (isset($_POST['log']) && isset($_POST['password'])){
-$log=$_POST['log'];
-$password=$_POST['password'];
-$data_born=$_POST['data_born'];
-$query = "INSERT INTO user (log,password,data_born) VALUES ('$log','$password','$data_born')";
-$result=mysqli_query($connect,$query);
+	$query="INSERT INTO user (data_born, log, password) VALUES('$data_born','$log', '$password')";
+	$result=mysqli_query($connect, $query);
 if($result){
-	$sm = "Регистрация прошла успешно";
-}
-else{
-	$fsm = "Ошибка";
-}
+		$zr="Регистрация прошла успешно";
+			} else {
+				$br="Ошибка";}
+			}
+			else { $ur="Пользователь с таким логином уже зарегистрирован";}
 
 
 
 }
+ ?>
 
-?>
-	<div class="container">
-		<form class="form-signin" method="POST">
-			<h2>Регистрация</h2>
-			<?php if(isset($sm)){?><div class="alert alert-success" role="alert"><?php echo $sm; ?></div><?php }?>
-			<?php if(isset($fsm)){?><div class="alert alert-success" role="alert"><?php echo $fsm; ?></div><?php }?>
+<div class="container">
+	<form class="reg" method="POST" >
+<h2> Регистрация</h2>
+<?php 
+if (isset($zr)){ ?> <div class="alert alert-success" role="alert"> <?php echo $zr;  ?> </div><?php  }?>
+<?php 
+if (isset($br)){ ?> <div class="alert alert-danger" role="alert"> <?php echo $br;  ?> </div><?php  }?>
+<?php 
+if (isset($ur)){ ?> <div class="alert alert-danger" role="alert"> <?php echo $ur;  ?> </div><?php  }?>
+
 			<label for="log" class="text-center pb-3">Введите ваш логин:</label>
 			<input type="text" name="log" class="form-control" placeholder="Имя пользователя" required="Заполните это поле."><br>
 			<label for="password" class="text-center pb-3">Введите ваш пароль:</label>
 			<input type="password" name="password" class="form-control" placeholder="Пароль" required="Заполните это поле."><br>
-			<label for="data_born" class="text-center pb-3">Дата рожения:</label>
+			<label for="data_born" class="text-center pb-3">Дата рождения:</label>
 			<input type="date" name="data_born" class="form-control" placeholder="Дата рождения" required="Заполните это поле."><br>
 			<button class="btn btn-lg btn-primary btn-block" type="submit">Регистрация</button>
-		</form>
-		
-	</div>
+
+
+
+	</form>
 </body>
 </html>
+
+
 <?php include "footer.php"; ?>
