09ddcd67a16f7fe7fc3d44a0e933d7cc56548d1b
Alexander <cherryknight@yandex.ru>
2019-05-18 00:32:56 +0300

    Добавлен вывод сообщения об ошибке авторизации в случае существования сессионной переменной error

--- loginform.php
+++ loginform.php
@@ -5,6 +5,11 @@
 	<div class="form-group">
 	    <input type="password" class="form-control" required name="password" placeholder="Пароль" >
 	</div>
+	<?php
+		if ($_SESSION['error']){
+			echo '<div class="alert alert-danger" role="alert">Неверное имя пользователя или пароль.</div>';
+			session_unset();}
+	?>
 	<div class="form-group">
 		<a href="registration">Впервые на сайте?</a>
 	</div>
