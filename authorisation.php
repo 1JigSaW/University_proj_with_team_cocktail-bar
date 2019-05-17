658a8ae127109bde26eefe3beda24500ce1d6695
Alexander <cherryknight@yandex.ru>
2019-05-18 00:37:35 +0300

    Если введенные логин/пароль не соответствуют ни одному зарегистрированному пользователю, оздет сессионную переменную error

--- authorisation.php
+++ authorisation.php
@@ -1,12 +1,11 @@
 <?php
-
 include 'connect_bd.php';
 if (!empty($_POST["login"]) && !empty($_POST["password"]))
 {
 	$result=mysqli_query($connect,"SELECT id,log FROM user WHERE log = '".$_POST["login"]."' AND password='".$_POST["password"]."'limit 1;") or die("...".mysqli_error($connect));
+		session_start();
 	if (mysqli_num_rows($result))
 	{
-		session_start();
 		$row=mysqli_fetch_assoc($result);
 		$_SESSION['user'] = $row['id'];
 		$_SESSION['log'] = $row['log'];
@@ -14,8 +13,10 @@ if (!empty($_POST["login"]) && !empty($_POST["password"]))
 	}
 	else
 	{
+		$_SESSION['error'] = 'asdas';
 		header("Location: login");
-		exit;
+		exit();
+		
 	}
 }
 
