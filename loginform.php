5281b50c617701c4bf649bb1caf27e30ad8a7bec
Alexander <cherryknight@yandex.ru>
2019-05-18 22:30:45 +0300

    При неудачной авторизации введенный логин в остается в поле

--- loginform.php
+++ loginform.php
@@ -1,11 +1,15 @@
 <?php
 	if ($_SESSION['error']){
 			echo '<div class="alert alert-danger" role="alert">Неверное имя пользователя или пароль.</div>';
+			$failedusername=$_SESSION['error'];
 			session_unset();}
 ?>
 <form method="POST" action="authorisation.php" class="text-center">
   	<div class="form-group">
-    	<input type="text" class="form-control" required name="login" placeholder="Имя пользователя">
+  		<?php  
+  		 	echo '<input type="text" class="form-control" required name="login" placeholder="Имя пользователя" value="'.$failedusername.'">';
+  		?>
+    	
 	</div>
 	<div class="form-group">
 	    <input type="password" class="form-control" required name="password" placeholder="Пароль" >
