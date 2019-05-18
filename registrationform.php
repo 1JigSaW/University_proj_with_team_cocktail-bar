205ae88cab56cd633d3e758c9a84f04099c3406d
Alexander <cherryknight@yandex.ru>
2019-05-18 11:19:12 +0300

    Изменен внешний вид формы регистрации

--- registrationform.php
+++ registrationform.php
@@ -1,26 +1,29 @@
 <?php require ('header.php');?>
 <title>Регистрация</title>
-<style >
-	.reg{
-		max-width: 400px;
-		padding: 15px;
-		margin: 0 auto;
-
-}
-</style>
-<form class="reg" method="POST" >
-<h2>Регистрация</h2>
-<?php
-if ($msg)
-	echo '<div class="alert alert-danger" role="alert">'.$msg.'</div>';
-?>		
-	<label for="log" class="text-center pb-3">Введите ваш логин:</label>
-	<input type="text" name="log" class="form-control" placeholder="Имя пользователя" required="Заполните это поле."><br>
-	<label for="password" class="text-center pb-3">Введите ваш пароль:</label>
-	<input type="password" name="password" class="form-control" placeholder="Пароль" required="Заполните это поле."><br>
-	<label for="data_born" class="text-center pb-3">Дата рождения:</label>
-	<input type="date" name="data_born" class="form-control" placeholder="Дата рождения" required="Заполните это поле."><br>
-	<button class="btn btn-lg btn-primary btn-block" type="submit">Регистрация</button>
+<h3 class="text-center pb-3">Регистрация</h3>
+<form method="POST" class="">
+	<?php
+	if ($msg)
+		echo '<div class="alert alert-danger" role="alert">'.$msg.'</div>';
+	?>
+  	<div class="form-group">
+  		<label for="log">Введите ваш логин:</label>
+    	<input type="text" name="log" class="form-control" placeholder="Имя пользователя" required="Заполните это поле.">
+	</div>
+	<div class="form-group">
+		<label for="password">Введите ваш пароль:</label>
+	    <input type="password" name="password" class="form-control" placeholder="Пароль" required="Заполните это поле.">
+	</div>
+	<div class="form-group">
+		<label for="data_born">Дата рождения:</label>
+	    <input type="date" name="data_born" class="form-control" placeholder="Дата рождения" required="Заполните это поле.">
+	</div>
+	<div class="container">
+  		<div class="row">
+		    <div class="col-12 text-center">
+		      <button type="submit" class="btn block btn-primary center">Регистрация</button>
+		    </div>
+  		</div>
+	</div>
 </form>
-
 <?php include "footer.php";?>
