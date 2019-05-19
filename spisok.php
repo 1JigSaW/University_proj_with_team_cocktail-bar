f1b4f416a62bbd18e4d43750b1d4c55d0d73054e
Alexander <cherryknight@yandex.ru>
2019-05-19 23:37:44 +0300

    Изменен размер заголовка страницы

--- spisok.php
+++ spisok.php
@@ -1,22 +1,17 @@
 <?php include "header.php";
 include "connect_bd.php";?>
 <title>Все статьи</title>
-<div>
-	<h1 class="display-4 text-center ">СПИСОК ВСЕХ ДОСТУПНЫХ СТАТЕЙ</h1>		
-	<?php
-	
-	$res = mysqli_query($connect,"SELECT * FROM `cocktail`")or die(mysqli_error());
+<h1 class="display-4 text-center pb-3">СПИСОК ВСЕХ ДОСТУПНЫХ СТАТЕЙ</h1>		
+<?php
+$res = mysqli_query($connect,"SELECT * FROM `cocktail`")or die(mysqli_error());
 
-	while($row = mysqli_fetch_assoc($res)){
-		echo "<div class=\"text-center\" ><a class='container-fluid h1' href='article_". $row['id']. "'>";
-		$id=$row['id'];
-		echo $row['title_coctail'];
-		echo "</a></div>";
-
-	}
-
-	mysqli_close($connect);
-	?> 
-</div>
+while($row = mysqli_fetch_assoc($res)){
+	echo "<div class=\"text-center\" ><a class='container-fluid h1' href='article_". $row['id']. "'>";
+	$id=$row['id'];
+	echo $row['title_coctail'];
+	echo "</a></div>";
+}
+mysqli_close($connect);
+?> 
 <?php include "footer.php" ?>
 
