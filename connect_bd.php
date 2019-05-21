4c307bc7a049a14ef1c0f6e0099ed048d3dea82e
Alexander <cherryknight@yandex.ru>
2019-05-21 23:15:32 +0300

    Вжух, и сайт не работает

--- connect_bd.php
+++ connect_bd.php
@@ -3,7 +3,7 @@
 $host='localhost';
 $user='root';
 $password='';
-$db_name='db_coctailbar';
+$db_name='db_cocktailbar';
 
 $connect=mysqli_connect($host,$user,$password,$db_name) or die("Ошибка " . mysqli_error($connect));
 mysqli_set_charset($connect,'utf8');
