d298ed6e3f76b24c2cbbdcb6f05a6e2f3e6394b8
Jig <slowlow1@rambler.ru>
2019-05-22 19:03:26 +0300

    Снёс уже не нужные файлы скриптов

--- script_output_bd_article.php
+++
@@ -1,9 +0,0 @@
-<?php 
-						$id=$_GET['page'];
-						$article = mysqli_query($link,"SELECT * FROM `article` WHERE $id=`cocktail_id`")or die(mysqli_error());	
-						while($arr = mysqli_fetch_assoc($article)){
-						{
-						echo $arr['rating'];
-						echo "<br>";}
-					}
-						?>
