ddee10bc0c1be0c3464bb2d49de12e5de69b0760
Alexander <cherryknight@yandex.ru>
2019-05-22 11:53:41 +0300

    Добавлены следующие ограничения: а) Логин должен состоять из 3-15 строчных символов латинского алфавита. Также допускатся цифры и символ нижнего подчеркивания. б) Пароль должен состоять из 6-15 символов

--- registration.php
+++ registration.php
@@ -1,13 +1,18 @@
 <?php 
 include ('connect_bd.php');
 if (isset($_POST['log']) && isset($_POST['password']))
-	{$data_born=strtotime($_POST['data_born']);
+	{$log=$_POST['log'];
+	$password=$_POST['password'];
+	$date=$_POST['data_born'];
+	$data_born=strtotime($date);
 	if (!$data_born ||($data_born > mktime(0, 0, 0, date("m"),date("d"),date("Y"))||($data_born < mktime(0, 0, 0, date("m"),date("d"),date("Y")-130))))
 		$msg='Некорректная дата рождения';
-	else{
-		if ($data_born<=mktime(0, 0, 0, date("m"),date("d"),date("Y")-18)){
-			$log=$_POST['log'];
-			$password=$_POST['password'];
+	elseif (!(preg_match('|[_\w&&[^A-Z]]{3,15}|',$log)))
+		$msg='Логин должен состоять из 3-15 строчных символов латинского алфавита. Также допускатся цифры и символ нижнего подчеркивания.';
+	elseif (!(preg_match('|.{6,15}|',$password)))
+		$msg='Пароль должен состоять из 6-15 символов.';
+	elseif ($data_born<=mktime(0, 0, 0, date("m"),date("d"),date("Y")-18)){
+			
 			$s="SELECT * FROM user WHERE log = '$log'";
 			$res=mysqli_query( $connect, $s);
 			$num = mysqli_num_rows($res);
@@ -23,11 +28,10 @@ if (isset($_POST['log']) && isset($_POST['password']))
 				$msg="Ошибка";
 						}
 						else {$msg="Пользователь с таким логином уже зарегистрирован";}}
-			else {
-				$msg='Регистрация доступна только пользователям старше 18 лет';}
+	else 
+		$msg='Регистрация доступна только пользователям старше 18 лет';
 		}
-	}
+
 if (!$suc) 
 	include 'registrationform.php';
-
 ?>
