e21fed816b8b3f07dc5b20b665c6fa4d5f1fbdcf
Alexander <cherryknight@yandex.ru>
2019-05-22 23:15:56 +0300

    Адаптивность блока "Популярное"

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 </div>		
 				</div>
 				<div class="col-md-3 p-0">
-					<div class="container p-2 my-auto text-white rounded" style="min-width:200px; background-color:rgba(0,75,75,.9);">
-						<h6 class="text-center ">Популярные статьи</h6>
-						<div class="row p-0 mx-2 my-2">
+					<div class="container p-1 p-sm-2 my-auto text-white rounded" style="min-width:200px; background-color:rgba(0,75,75,.9);">
+						<h6 class="text-center">Популярные статьи</h6>
+						<div class="row p-0 m-1 md-2">
 							<?php
 								include "random_articles.php";
 								include "connect_bd.php";
 								$return=popular_range($connect);
 								$query=
-										"<a class='col col-md-12 %s text-center popular' href='/article_%d'><div class='row align-items-center text-white' style='height:144px; background:url(img/articles/covers/%s);background-size: cover;background-position: center;'><div class='col item'><p style='text-shadow:black 1px 1px 0, black -1px -1px 0,black -1px 1px 0, black 1px -1px 0;'>%s</p></div></div></a>
+										"<a class='col col-md-12 %s text-center popular' href='/article_%d'><div class='row rounded align-items-center text-white' style='height:144px; background:url(img/articles/covers/%s);background-size: cover;background-position: center;'><div class='col item'><p style='text-shadow:black 1px 1px 0, black -1px -1px 0,black -1px 1px 0, black 1px -1px 0;'>%s</p></div></div></a>
 										";
-								$m=array('mr-2 mr-md-0 mb-md-2','ml-2 ml-md-0 mt-md-2');
+								$m=array('mr-0 mr-sm-1 mr-md-0 mb-md-1','ml-2 ml-md-0 mt-md-2');
 								for($i=0;$i<2;$i++){
 									printf($query,$m[$i],$return[$i]['id'],$return[$i]['img'],$return[$i]['title_coctail']);
 								}
