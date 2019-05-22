c91e7f3ea61bc5d47985d050d2cb20bd53631892
Alexander <cherryknight@yandex.ru>
2019-05-22 22:06:02 +0300

    Адаптивная шапка

--- header.php
+++ header.php
@@ -30,37 +30,35 @@ if(@$_GET['do'] == 'logout')
 			<div class="row">
 				<div class="col my-auto p-0 m-0">
 					<div class="btn-group p-1 m-0 float-right">
-						<a href="/" role="button" class="btn btn-lg bg-primary text-white border"><span class="oi oi-home"></span></a>
-						<a href="/spisok" role="button" class="btn btn-lg bg-primary text-white border mx-1">Статьи</a>
+						<a href="/" role="button" class="btn btn-lg bg-primary rounded text-white border-white"><span class="oi oi-home"></span></a>
+						<a href="/spisok" role="button" class="btn btn-lg bg-primary rounded text-white border mx-1">Статьи</a>
 						<div class="d-none d-sm-block">
 							<a href="/about" role="button" class="btn btn-lg bg-primary text-white border">О нас</a>
 						</div>
 						<div class="d-sm-none">
-							<a href="/about" role="button" class="btn btn-lg text-white px-3 py-2"><span class="oi oi-info"></span></a>
+							<a href="/about" role="button" class="btn btn-lg text-white p-0 m-0 mx-2 py-2"><span class="oi oi-info"></span></a>
 						</div>
 					</div>
 				</div>
-				<div class="col my-auto d-none d-sm-block">
+				<div class="col my-auto p-0">
 					    <form action='/search' method="get" class="form-inline float-right">
 					       <input class="form-control p-0 m-0" name="q" type="text" class="input-medium search-query" placeholder=" Search...">
 						</form>
 				</div>
-				<div class="col d-sm-none py-1 px-0">
-					<a href="#" role="button" class="btn btn-lg text-white pl-0 pr-2 mx-0"><span class="oi oi-magnifying-glass"></span></a>
-				</div>
-				<div class="container col p-0 m-0 btn-group my-auto">
+				<div class="col-auto col-md p-0 m-0 btn-group my-auto">
 					<div class="container text-white text-center d-none d-sm-none d-md-block p-0 m-0">
 					<?php
+						$logout='<a href="?do=logout" role="button" class="p-0 m-0 btn btn-lg text-white"><span class="oi oi-account-logout"></span></a>';
 						if (!@$_SESSION['user']){
-							echo '<a href="/login" role="button" class="btn  border-white rounded text-white mr-1" style="">Вход</a>';
+							echo '<a href="/login" role="button" class="btn border-white rounded text-white mr-1" style="">Вход</a>';
 							echo '<a href="/registration" role="button" class="btn  border-white rounded text-white">Регистрация</a>';}
 						else
 							{
 							echo '<ul class="list-inline my-auto"><li class="list-inline-item">Вы вошли как <b>'.$_SESSION['log'].'</b></li>';
-							echo '<a href="?do=logout" role="button" class="p-0 m-0 btn btn-lg text-white"><span class="oi oi-account-logout"></span></a>';}
+							echo $logout;}
 					?>
 					</div>
-					<div class="d-md-none p-0 m-auto">
+					<div class="d-md-none float-right p-0 mx-2 bg-info">
 						<?php
 						if (!@$_SESSION['user'])
 							echo '<a href="/login" role="button" class="p-0 m-0 btn btn-lg mr-1 text-white"><span class="oi oi-account-login"></span></a>';
