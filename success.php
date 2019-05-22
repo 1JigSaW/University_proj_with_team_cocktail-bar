117d8bf319c557302f41f51344f03a3cbb9d7999
thienlao <tiger07992222@gmail.com>
2019-05-23 00:36:29 +0300

    Поменял шрифт заголовка на странице успешной регистрации

--- success.php
+++ success.php
@@ -3,7 +3,7 @@
 echo '<meta http-equiv="refresh" content="2;URL=/login">';
 ?>
 	<title>Спасибо за регистрацию</title>
-	<h1 class="display-4 text-center pb-3">Регистрация прошла успешно!</h1>
+	<h1 class="display-4 text-center pb-1 pb-md-3">Регистрация прошла успешно!</h1>
 	<p class="text-center">
 		Сейчас вы будете перенаправлены на страницу авторизации.
 	</p>
